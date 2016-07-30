2c6caeb40c4d89e837a6282664eb2d88254dfa74
Jaime5 <Jaime5@UMBC.edu>
2016-07-30 06:13:00 -0400

    add all files

--- personal_site/personal_site/settings.py
+++ personal_site/personal_site/settings.py
@@ -24,7 +24,9 @@ SECRET_KEY = '(iha!&)(44ilvd3-levreipg2ck_geu4t-5$0t2)q*pyg6a&#r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+	'*'
+]
 
 # Application definition
 
@@ -113,3 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR, 'static')
+]
