44e7cfbef268d18dc9ac46d005e39a01dd0f6468
Jaime5 <Jaime5@UMBC.edu>
2016-07-30 06:32:42 -0400

    move folder configurations for heroku

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'personal_site.urls'
 TEMPLATES = [
 	{
 		'BACKEND': 'django.template.backends.django.DjangoTemplates',
-		'DIRS': ['personal_site/templates'],
+		'DIRS': ['templates'],
 		'APP_DIRS': True,
 		'OPTIONS': {
 			'context_processors': [
